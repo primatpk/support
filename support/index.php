58287978926c9cbcd638203a27d85e4431947f2f
brwniess <brwniess@yahoo.com>
2022-10-24 11:08:03 +0700

    Edit Halaman Depan

--- support/index.php
+++ support/index.php
@@ -38,7 +38,7 @@ if ($cfg && $cfg->isKnowledgebaseEnabled()) { ?>
     if($cfg && ($page = $cfg->getLandingPage()))
         echo $page->getBodyWithImages();
     else
-        echo  '<h1>'.__('Welcome to the Support Center').'</h1>';
+        echo  '<h1>'.__('Selamat datang di Aplikasi Ticketing PT Prima Terminal Petikemas').'</h1>';
     ?>
     </div>
 </div>
